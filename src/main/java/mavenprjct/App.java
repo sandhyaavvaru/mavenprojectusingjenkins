f4124aeb401ff1f7e9a4bb02aca37327af447e85
EC2 Default User <ec2-user@ip-172-31-44-215.ap-south-1.compute.internal>
2024-06-24 09:48:57 +0000

    good girl

--- src/main/java/mavenprjct/App.java
+++ src/main/java/mavenprjct/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!I am Sandhya I am honest and cute" );
+        System.out.println( "Hello World!I am Sandhya I am very good girl" );
     }
 }
